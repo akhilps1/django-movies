f25ce638419f86a59771d1abb1c9e62bb8ba6cc8
akhilps <akhilps595@gmail.com>
2022-11-19 16:32:09 +0530

    movies model created

--- movie_details/details/models.py
+++ movie_details/details/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# model for create a new movie
+class MovieDetails(models.Model):
+    movie_name = models.CharField(max_length=250)
+    movie_description = models.TextField()
+    release_date = models.DateField()
+    movie_duration = models.CharField(max_length=100)
+    
+
+    def __str__(self) -> str:
+        return f"{self.movie_name}"
+    
